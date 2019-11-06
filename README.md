4e93806016d75226f2446a19495dac43f1a4d128
Benjamin Bulis <bbulis@student.tgm.ac.at>
2019-11-06 18:05:06 +0100

    Added Readme.md and Comments to Code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Schülerverwaltung
+
+## Aufgabenstellung
+
+Zu erstellen war eine Schülerverwaltung, welche Daten über Schüler speichert. Neue Daten können hinzugefügt werden und wieder gelöscht werden. Das ganze soll in Vue.JS geschrieben sein und nur am Client laufen. 
+
+## Lauffähig machen
+
+Die Software ist sofort Lauffähig. Es müssen keine weiteren Inhalte manuell herunter geladen werden. Es wird jedoch eine Internetverbindung benötigt, damit eine Verbindung zum CDN hergestellt werden kann.
+
+#### Copyrights
+
+Angabe Prof. Christoph Roschger
+
+Code Benjamin Bulis
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {Vorname des Schülers} firstname 
+ * @param {Nachname des Schülers} lastname 
+ * @param {Klasse in welcher der Schüler ist} clazz 
+ * @param {Ausbildungszweig welcher vom Schüler gewählt wurde} branch 
+ */
 function Schueler(firstname, lastname, clazz, branch) {
     this.firstname = firstname;
     this.lastname = lastname;
@@ -5,26 +12,39 @@ function Schueler(firstname, lastname, clazz, branch) {
     this.branch = branch;
 }
 
+/**
+ * Array welches die Schüler speichert
+ */
 let schuelers = [
     new Schueler('Benjamin', 'Bulis', '5CHIT', 'MEDT'),
     new Schueler('Oguzhan', 'Güngür', '5CHIT', 'MEDT'),
     new Schueler('David', 'Langheiter', '5CHIT', 'SYT')
 ];
 
+/**
+ * Logik von Vue.JS
+ */
 let app = new Vue({
+    // Element auf welches ich im HTML Code zugreifen will
     el: '#app',
+    // Daten welche ich dem HTML Code bereitstellen möchte
     data: {
+        // Daten welche mit dem v-model zusammenhängen
+        // Der name hier verweißt auf eine Variable im HTML Code -> v-model
         firstname: '',
         lastname: '',
         clazz: '',
         branch: '',
         schuelers
     },
+    // Methoden welche vom HTML Code aus aufgerufen werden können
     methods: {
+        // Funktion welche aufgerufen wird sobald der Hinzufügen Knopf gedrückt wird
         addSchueler: function() {
             let newschueler = new Schueler(this.firstname, this.lastname, this.clazz, this.branch);
             schuelers.push(newschueler);
         },
+        // Funktion welche ein Element aus der Liste entfernt
         deleteSchueler: function(inputschueler) {
             let index = schuelers.indexOf(inputschueler);
             schuelers.splice(index, 1);
